b129bc5c649b5d39df5e701b83e9573ac823f673
Egan Jasmine <jcegan92@gmail.com>
2022-10-20 16:39:12 -0700

    working RPS

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 var computerChoice = 0
 function getComputerChoice() {
-    computerChoice = Math.floor(Math.random() * 3)
-    console.log(computerChoice)
+    computerChoice = Math.floor(Math.random() * 3);
+    console.log(computerChoice);
     if (computerChoice == 0) {
-        computerChoice = 'ROCK'
-        console.log(computerChoice)
+        computerChoice = 'ROCK';
+        console.log(computerChoice);
 
     } if (computerChoice == 1) {
-        computerChoice = 'PAPER'
-        console.log(computerChoice)
+        computerChoice = 'PAPER';
+        console.log(computerChoice);
     } if (computerChoice == 2) {
-        computerChoice = 'SCISSORS'
-        console.log(computerChoice)
+        computerChoice = 'SCISSORS';
+        console.log(computerChoice);
     }
-    return computerChoice
+    return computerChoice;
 }
 getComputerChoice()
 var result = ''
 function readyFight (playerSelection, computerChoice) {
-    playerSelection.toUpperCase()
+    playerSelection = playerSelection.toUpperCase();
     if (playerSelection == computerChoice) {
-        result = 'Tie'
-        else if (playerSelection == 'ROCK' && computerChoice =='SCISSORS') || (playerSelection == 'PAPER' && computerChoice == 'ROCK' || (playerSelection = 'SCISSORS' && computerChoice == 'PAPER')
-
+        result = 'Tie';
+    } else if ((playerSelection == 'ROCK' && computerChoice =='SCISSORS') ||
+            (playerSelection == 'PAPER' && computerChoice == 'ROCK') ||
+            (playerSelection == 'SCISSORS' && computerChoice == 'PAPER')) {
+        result = 'winner';
+    } else {result = 'loser';}
+    console.log(result)
+    return result
     }
-}
-readyFight('ROCK', computerChoice)
+readyFight('paper', computerChoice)
